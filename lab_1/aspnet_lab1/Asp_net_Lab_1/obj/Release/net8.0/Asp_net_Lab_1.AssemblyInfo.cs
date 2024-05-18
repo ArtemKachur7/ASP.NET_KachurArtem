573c2fe3c695be0c71ba903d084d2c88455ee3bf
ArtemKachur7 <kachurartem7@gmail.com>
2024-05-18 09:36:52 +0300

    lab1-lab2

--- lab_1/aspnet_lab1/Asp_net_Lab_1/obj/Release/net8.0/Asp_net_Lab_1.AssemblyInfo.cs
+++ lab_1/aspnet_lab1/Asp_net_Lab_1/obj/Release/net8.0/Asp_net_Lab_1.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Asp_net_Lab_1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+540c7453572348221b4faa65b82e9ea925831abc")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9c33a7257f5b7de21ea21acaafd2f7b275d8c123")]
 [assembly: System.Reflection.AssemblyProductAttribute("Asp_net_Lab_1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Asp_net_Lab_1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
